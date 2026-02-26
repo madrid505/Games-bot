8f7495df669f4c0bd6869b29e917e00f5466a065
madrid505 <anassleet877@gmail.com>
2026-02-26 22:05:58 +0300

    Delete core/permissions.py

--- core/permissions.py
+++
@@ -1,9 +0,0 @@
-from telegram import Update
-from telegram.ext import ContextTypes
-from config import OWNER_ID
-
-async def is_owner_or_admin(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    user = update.effective_user
-    chat = update.effective_chat
-    member = await chat.get_member(user.id)
-    return user.id == OWNER_ID or member.status in ['administrator', 'creator']
