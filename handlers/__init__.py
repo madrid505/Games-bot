8fb0becc858e9c20740c38cd693e8feeceeb6117
madrid505 <anassleet877@gmail.com>
2026-02-27 02:12:14 +0300

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,23 +1,9 @@
 from telegram.ext import MessageHandler, CallbackQueryHandler, filters
 from .games_handler import handle_messages, callback_handler
-from .bank_handler import bank_logic
-from .roulette_handler import roulette_logic
-from .interaction_handler import interaction_logic
 
 def register_handlers(app):
-    # استخدام المجموعات (group) يضمن أن كل ملف يستلم الرسالة ويعالجها بشكل مستقل
+    # إضافة معالج الرسائل النصية
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_messages))
     
-    # المجموعة 1: ملك التفاعل (يجب أن يكون أولاً ليحسب كل رسالة)
-    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, interaction_logic), group=1)
-    
-    # المجموعة 2: أوامر البنك (رصيدي، راتب، زرف...)
-    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, bank_logic), group=2)
-    
-    # المجموعة 3: الروليت (انا، تم، روليت)
-    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, roulette_logic), group=3)
-    
-    # المجموعة 4: الألعاب (تشغيل الألعاب والتحقق من الإجابات)
-    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_messages), group=4)
-    
-    # أزرار القائمة
+    # إضافة معالج الأزرار (القائمة)
     app.add_handler(CallbackQueryHandler(callback_handler))
