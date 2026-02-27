a50c4f93f1dfd9d45040b07e8724ed18e2bec9f2
madrid505 <anassleet877@gmail.com>
2026-02-27 22:39:29 +0300

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,6 +1,1 @@
-from .games_handler import handle_messages, callback_handler
 
-# هذا الملف يعمل كجسر لربط ملف الألعاب والبنك والفعاليات
-# بحيث يقرأ ملف main.py الوظائف الأساسية مباشرة
-
-__all__ = ['handle_messages', 'callback_handler']
