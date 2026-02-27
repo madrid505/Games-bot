6c4e8a983a87f2e028536dd03cd671744582017b
madrid505 <anassleet877@gmail.com>
2026-02-27 22:31:51 +0300

    Update interaction_handler.py

--- handlers/interaction_handler.py
+++ handlers/interaction_handler.py
@@ -1,7 +1,11 @@
 from db import get_user_data, db, User
 
 async def update_interaction(update, u_id):
-    u_data = await get_user_data(update)
-    current_count = u_data.get('msg_count', 0) + 1
-    db.update({'msg_count': current_count}, User.id == u_id)
-    return current_count
+    try:
+        u_data = await get_user_data(update)
+        current_count = u_data.get('msg_count', 0) + 1
+        db.update({'msg_count': current_count}, User.id == u_id)
+        return True
+    except Exception as e:
+        print(f"❌ خطأ في ملك التفاعل: {e}")
+        return False
