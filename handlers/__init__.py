6e7fb00c5afb4197d0e70bd032c3886d5634aab0
madrid505 <anassleet877@gmail.com>
2026-02-27 23:22:18 +0300

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,4 +1,1 @@
-from .interaction_handler import update_interaction
-from .games_handler import handle_messages, callback_handler
-from .bank_handler import handle_bank
-from .roulette_handler import handle_roulette
+
