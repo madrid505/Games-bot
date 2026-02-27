0063fd4a78c8f8d935a1e2a259b1194913eba67f
madrid505 <anassleet877@gmail.com>
2026-02-27 18:09:35 +0300

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,9 +1,6 @@
-from telegram.ext import MessageHandler, CallbackQueryHandler, filters
 from .games_handler import handle_messages, callback_handler
 
-def register_handlers(app):
-    # إضافة معالج الرسائل النصية
-    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_messages))
-    
-    # إضافة معالج الأزرار (القائمة)
-    app.add_handler(CallbackQueryHandler(callback_handler))
+# هذا الملف يعمل كجسر لربط ملف الألعاب والبنك والفعاليات
+# بحيث يقرأ ملف main.py الوظائف الأساسية مباشرة
+
+__all__ = ['handle_messages', 'callback_handler']
