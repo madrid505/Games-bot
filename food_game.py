53b6ae7fbe3279155811100494c3d48a7c5a0d1b
madrid505 <anassleet877@gmail.com>
2026-08-22 14:12:35 +0300

    Add Arabic food game data
    This file contains a list of Arabic dishes for the game 'Guess the Dish'. Each dish has a name and a corresponding file ID.

---
+++ food_game.py
@@ -0,0 +1,53 @@
+# ==========================================
+# بنك صور الأطباق العربية - لعبة "خمن الأكلة"
+# ==========================================
+ARABIC_FOOD_GAME = [
+    {
+        "name": "صيادية سمك",
+        "file_id": "AgACAgQAAxkBAAIRZWqJgebeODAKLypD3CfVKxv-w53JAAIkGWsb_sFIUN5zGwcioygXAQADAgADbQADPQQ="
+    },
+    {
+        "name": "مشاوي",
+        "file_id": "AgACAgQAAxkBAAIRZ2qJghH8YvJu5ktk_FLgnyaLeCQZAAIlGWsb_sFIUDwTXCWnTHzPAQADAgADbQADPQQ="
+    },
+    {
+        "name": "مقلوبة",
+        "file_id": "AgACAgQAAxkBAAIRaWqJginH9vodibGi5rSa-D8Z8zmUAAImGWsb_sFIULfvJm4cjDGCAQADAgADeAADPQQ="
+    },
+    {
+        "name": "صاجية",
+        "file_id": "AgACAgQAAxkBAAIRa2qJgj_OEx8X5yAukZnkcgABGUwQfwACJxlrG_7BSFCorSSxbB-xkwEAAwIAA20AAz0E="
+    },
+    {
+        "name": "الشيخ مخشي",
+        "file_id": "AgACAgQAAxkBAAIRbWqJgk6bLvIPAsXuRFCilNbC9rP6AAIoGWsb_sFIUMGPdWjdPb9CAQADAgADbQADPQQ="
+    },
+    {
+        "name": "كبسة",
+        "file_id": "AgACAgQAAxkBAAIRb2qJgl-2krM7FM5skzRhne82hH0lAAIpGWsb_sFIUBEx5R9csq0rAQADAgADeQADPQQ="
+    },
+    {
+        "name": "ملوخية",
+        "file_id": "AgACAgQAAxkBAAIRcWqJgnj4Vfwo1v7w2JdfwrT5clr1AAIqGWsb_sFIUNP-0W8hjXcQAQADAgADeAADPQQ="
+    },
+    {
+        "name": "مسخن",
+        "file_id": "AgACAgQAAxkBAAIRc2qJgpC9GMYieT8mfXxDvwofvIA-AAIrGWsb_sFIUOf4BDzF9q1mAQADAgADeAADPQQ="
+    },
+    {
+        "name": "مندي",
+        "file_id": "AgACAgQAAxkBAAIRdWqJgqUaFRX4AnP4u1Ah-dst7_sLAAIsGWsb_sFIUNlD7m7kG2mMAQADAgADeAADPQQ="
+    },
+    {
+        "name": "منسف",
+        "file_id": "AgACAgQAAxkBAAIRd2qJgre8LvpBXekyiT8zJkhw2LmSAAItGWsb_sFIUOxWeV07RvPSAQADAgADeQADPQQ="
+    },
+    {
+        "name": "ورق عنب",
+        "file_id": "AgACAgQAAxkBAAIReWqJgvhe0X4jCLjzc-5qq3e9FbQ6AAIvGWsb_sFIUM8Rxkxg3wqQAQADAgADbQADPQQ="
+    },
+    {
+        "name": "قدرة",
+        "file_id": "AgACAgQAAxkBAAIRe2qJgznQ2-IGakn51U1Fe1favz_VAAIwGWsb_sFIUBBtNKH2NSEHAQADAgADbQADPQQ="
+    }
+]
